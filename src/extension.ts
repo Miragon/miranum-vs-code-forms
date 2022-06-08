6be741c41ce59ca6cfa5a90a55c0c44f5d80d5d0
Peter Heinemann <peter.heinemann@miragon.io>
2022-06-08 13:10:23 +0200

    .form files with json syntax highlighting

--- src/extension.ts
+++ src/extension.ts
@@ -2,5 +2,17 @@ import * as vscode from 'vscode';
 import { JsonSchemaBuilderProvider } from './jsonSchemaBuilderProvider';
 
 export function activate(context: vscode.ExtensionContext) {
+
+	// Todo: Make sure the .vscode/settings.json file exist with the files.associations entry.
+	// To use the json highlighting for .form files we update the workspace settings
+	// This only works when a workspace exists with the .vscode/settings.json file and
+	// the file contains the files.associations config.
+	const languages = vscode.workspace.getConfiguration('files').inspect<Object>('associations');
+	if (languages?.workspaceValue && !('*.form' in languages.workspaceValue)) {
+		const newAssociation: {[key: string]: any} = languages.workspaceValue;
+		newAssociation['*.form'] = 'json';
+		vscode.workspace.getConfiguration('files').update('associations', newAssociation);
+	}
+
 	context.subscriptions.push(JsonSchemaBuilderProvider.register(context));
 }
