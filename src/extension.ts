5b4377144943870b9e2438677123578f8bf47d42
Peter Heinemann <peter.heinemann@miragon.io>
2022-06-09 10:20:39 +0200

    create settings.json when it's missing

--- src/extension.ts
+++ src/extension.ts
@@ -3,15 +3,17 @@ import { JsonSchemaBuilderProvider } from './jsonSchemaBuilderProvider';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	// Todo: Make sure the .vscode/settings.json file exist with the files.associations entry.
-	// To use the json highlighting for .form files we update the workspace settings
-	// This only works when a workspace exists with the .vscode/settings.json file and
-	// the file contains the files.associations config.
+	// To handle .form files as .json files we add or create a new config.
 	const languages = vscode.workspace.getConfiguration('files').inspect<Object>('associations');
-	if (languages?.workspaceValue && !('*.form' in languages.workspaceValue)) {
-		const newAssociation: {[key: string]: any} = languages.workspaceValue;
-		newAssociation['*.form'] = 'json';
-		vscode.workspace.getConfiguration('files').update('associations', newAssociation);
+	if (languages?.workspaceValue) {
+		if (!('*.form' in languages.workspaceValue)) {
+			const newAssociation: {[key: string]: any} = languages.workspaceValue;
+			newAssociation['*.form'] = 'json';
+			vscode.workspace.getConfiguration('files').update('associations', newAssociation);
+		}
+	} else {
+		// If the configuration is not set at all we create it.
+		vscode.workspace.getConfiguration('files').update('associations', {"*.form": "json"});
 	}
 
 	context.subscriptions.push(JsonSchemaBuilderProvider.register(context));
