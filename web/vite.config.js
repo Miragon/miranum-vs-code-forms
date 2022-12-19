43cf21697fad706b676c68aaed3ddd0bf00d10dc
jakobmertl <jakob.mertl@miragon.io>
2022-12-19 13:39:09 +0100

    feat/Miranum: reset config

--- web/vite.config.js
+++ web/vite.config.js
@@ -26,8 +26,7 @@ export default defineConfig({
         viteStaticCopy({
             targets: [
                 {src: 'node_modules/@mdi/font/css/**', dest: 'assets/css/'},
-                {src: 'node_modules/@mdi/font/fonts/**', dest: 'assets/fonts/'},
-                {src: 'images/logo_marketplace.png', dest: 'assets/images/'}
+                {src: 'node_modules/@mdi/font/fonts/**', dest: 'assets/fonts/'}
             ]
         })
     ],
