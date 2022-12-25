67236f12e8c282f18198e7a7008279071820b4d7
Peter Heinemann <peter.heinemann@miragon.io>
2022-12-25 09:58:07 +0100

    fix: delete old renderer

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,12 +1,6 @@
 import {Form} from "@muenchen/digiwf-form-renderer";
-import {TextDocument} from "vscode";
 
 export type Schema  = {
     key: string
     schema: Form
 }
-
-export interface IDocumentWrapper {
-    document: TextDocument;
-}
-
